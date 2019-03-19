ce68445d69f98d1f4270f4f6ee69861950ead219
madhank93 <maddymadhan946@gmail.com>
2019-03-19 18:02:47 +0530

    Solution

--- S02.03-Exercise-Polish/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.03-Exercise-Polish/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -91,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPreExecute() {
+            super.onPreExecute();
             mProgressBar.setVisibility(View.VISIBLE);
         }
 
